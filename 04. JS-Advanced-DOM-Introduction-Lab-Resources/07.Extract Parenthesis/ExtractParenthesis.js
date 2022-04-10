b7f68badc0a32f62c41d74355149e490da7c2dc9
pavelhristov <hristov.pavel@zohomail.eu>
2022-04-10 20:12:01 +0300

    'Lab'

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+let textElement = document.getElementById('content');
+let pattern = /\((?<text>\w+\s?\w*\s*\w*)\)/gm
+let match = pattern.exec(textElement.textContent);
+let elementArr = [];
+while (match != null){
+elementArr.push(match.groups['text']);
+
+match = pattern.exec(textElement.textContent);
+}
+textElement.textContent = elementArr.join('; ')
 
 }
