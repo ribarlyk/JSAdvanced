bb59823c1b1b623fb368b3348f5cf786c36bdef9
pavelhristov <hristov.pavel@zohomail.eu>
2022-04-12 19:35:10 +0300

    Excercise

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+  let towns = Array.from(document.querySelectorAll("#towns li"));
+  let searchElement = document.getElementById("searchText").value;
+  counter = 0;
+
+  towns.forEach((element) => {
+    if (
+      element.textContent.includes(searchElement) &&
+      searchElement.length > 0
+    ) {
+      counter++;
+      element.style.fontWeight = "bold";
+      element.style.textDecoration = "underline";
+      console.log(searchElement);
+    } else {
+      element.style.fontWeight = "normal";
+      element.style.textDecoration = "none";
+    }
+  });
+  let result = document.querySelector("#result");
+  console.log(result.textContent);
+  result.textContent = `${counter} matches found`;
 }
