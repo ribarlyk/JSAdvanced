985f3226b0fa20a15c9cb6b81ccb422da6ed74c7
pavelhristov <hristov.pavel@zohomail.eu>
2022-04-14 13:27:05 +0300

    Excercise

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,15 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-
-   function onClick() {
-      //   TODO:
-
-   }
+   let dataTr = Array.from(document.querySelectorAll('tbody > tr'));
+   let searchBtn = document.querySelector("#searchBtn");
+   let searchField = document.querySelector("#searchField");
+   searchBtn.addEventListener('click', function () {
+       let regex = new RegExp(searchField.value, 'gim');
+       dataTr.map(e => {
+           e.classList.remove('select');
+           if (e.innerText.match(regex) !== null) {
+               e.className = 'select';
+           }
+       });
+       searchField.value = '';
+   });
 }
