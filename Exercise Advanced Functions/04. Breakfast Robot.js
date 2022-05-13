b43450a8bc4b6f8aff883c3cc3626e51529094c1
pavelhristov <hristov.pavel@zohomail.eu>
2022-05-13 19:02:37 +0300

    'Excercise'

---
+++ Exercise Advanced Functions/04. Breakfast Robot.js
@@ -0,0 +1,11 @@
+function solution(){
+    
+}
+
+
+
+
+let manager = solution ();
+console.log (manager ("restock flavour 50"));// Success 
+console.log (manager ("prepare lemonade 4"));
+console.log(manager('restock carbohydrate 10'))
