058e1763540e1ea7db531d5c5b2f882225d3da38
pavelhristov <hristov.pavel@zohomail.eu>
2022-05-26 23:29:44 +0300

    Excercise

---
+++ Objects and Composition - Exercise/02. Construction Crew.js
@@ -0,0 +1,15 @@
+
+    const worker = { weight: 80,
+        experience: 1,
+        levelOfHydrated: 0,
+        dizziness: true }
+function solve(worker){
+if(worker.dizziness){
+    worker.levelOfHydrated+= 0.1 * worker.weight * worker*experience;
+    worker.dizziness = false;
+}
+return worker
+}
+
+
+
