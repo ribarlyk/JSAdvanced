6f4464f9ea269707417f1159791a9167b3a9cd48
PavelHristov <hristov.pavel@zohomail.eu>
2022-05-26 12:23:49 +0300

    Lab

---
+++ Objects and Composition - Lab/4.Object Factory.js
@@ -0,0 +1,43 @@
+function factory(library, orders) {
+  let products = [];
+  for (let line of orders) {
+    let product = line.template;
+    for (let func of line.parts) {
+      product[func] = library[func];
+    }
+    products.push(product);
+  }
+  return products;
+}
+
+const library = {
+  print: function () {
+    console.log(`${this.name} is printing a page`);
+  },
+  scan: function () {
+    console.log(`${this.name} is scanning a document`);
+  },
+  play: function (artist, track) {
+    console.log(`${this.name} is playing '${track}' by ${artist}`);
+  },
+};
+const orders = [
+  {
+    template: { name: "ACME Printer" },
+    parts: ["print"],
+  },
+  {
+    template: { name: "Initech Scanner" },
+    parts: ["scan"],
+  },
+  {
+    template: { name: "ComTron Copier" },
+    parts: ["scan", "print"],
+  },
+  {
+    template: { name: "BoomBox Stereo" },
+    parts: ["play"],
+  },
+];
+const products = factory(library, orders);
+console.log(products);
