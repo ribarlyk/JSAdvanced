db4be8ad02b332a4359e63ae43cc841c32d6e557
PavelHristov <hristov.pavel@zohomail.eu>
2022-05-31 00:07:16 +0300

    Lab

--- 04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,13 +1,13 @@
 function sumTable() {
-    let items = document.querySelectorAll('tr td:nth-of-type(2)');
-    let sum = Array.from(items).reduce((a, x) => {
-        let currentSum = Number(x.textContent) || 0;
-        return a + currentSum;
-    }, 0)
-    let sumElement = document.getElementById('sum');
-    if (sumElement.textContent > 0) {
-        sum
-    } else {
-        sumElement.textContent = sum;
-    }
+  let items = document.querySelectorAll("tr td:nth-of-type(2)");
+  let sum = Array.from(items).reduce((a, x) => {
+    let currentSum = Number(x.textContent) || 0;
+    return a + currentSum;
+  }, 0);
+  let sumElement = document.getElementById("sum");
+  if (sumElement.textContent > 0) {
+    sum;
+  } else {
+    sumElement.textContent = sum;
+  }
 }
