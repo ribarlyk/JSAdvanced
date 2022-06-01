64a11497fa4054acb6db2b52ccceb7838b6fc883
pavelhristov <hristov.pavel@zohomail.eu>
2022-06-02 00:37:34 +0300

    'Lab'

--- DOM Manipulations and Events/05. Highlight-Active/app.js
+++ DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+  const sections = Array.from(document.querySelectorAll("div input"));
+
+  sections.forEach((el) => {
+    el.addEventListener("focus", onFocus);
+    el.addEventListener("blur", onBlur);
+  });
+
+  function onFocus(e) {
+    e.target.parentElement.classList.add("focused");
+  }
+  function onBlur(e) {
+    e.target.parentElement.classList.remove("focused");
+  }
 }
--- DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-    console.log('TODO:...');
+  let regex = /^\w*\@(\w*\.\w+)$/gm;
+  const inputElement = document.querySelector("#email");
+  inputElement.addEventListener("change", ifChange);
+
+  function ifChange() {
+    regex.test(inputElement.value)
+      ? inputElement.classList.remove("error")
+      : inputElement.classList.add("error");
+  }
 }
