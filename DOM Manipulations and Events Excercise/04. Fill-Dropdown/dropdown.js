792e90f70b9147a1dbcf413031d43ed6542120aa
PavelHristov <hristov.pavel@zohomail.eu>
2022-06-05 14:42:26 +0300

    'Excercse'

--- DOM Manipulations and Events Excercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events Excercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.querySelector("#newItemText")
+    let newItemValue = document.querySelector("#newItemValue")
+    let optionElement = document.createElement("option");
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newItemValue.value;
+    let selectItem = document.querySelector("#menu");
+    selectItem.appendChild(optionElement);
+    newItemText.value= '';
+    newItemValue.value = '';
 }
--- DOM Manipulations and Events Excercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulations and Events Excercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,34 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let inputText = document.getElementsByTagName("textarea")[0];
+    let outputText = document.getElementsByTagName("textarea")[1];
+    document
+        .getElementsByTagName("button")[0]
+        .addEventListener("click", encode);
+    document
+        .getElementsByTagName("button")[1]
+        .addEventListener("click", decode);
+
+
+    let encryptedMsg = "";
+    let decryptedMsg = "";
+
+    function encode(e) {
+        for (let i = 0; i < inputText.value.length; i++) {
+            let char = inputText.value[i].charCodeAt() + 1;
+            encryptedMsg += String.fromCharCode(char);
+        }
+
+        outputText.value = encryptedMsg;
+        inputText.value = "";
+        encryptedMsg = '';
+    }
+    function decode(e) {
+        for (let i = 0; i < outputText.value.length; i++) {
+            let char = outputText.value[i].charCodeAt() - 1;
+            console.log(char);
+            decryptedMsg += String.fromCharCode(char);
+        }
+        outputText.value = decryptedMsg;
+        decryptedMsg = '';
+    }
 }
